97557778b963440b36e4386540ab8fdd269cfef1
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-01-16 01:13:25 +0530

    Create script.js

---
+++ Themed-clock/script.js
@@ -0,0 +1,11 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
+
+const daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthsName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+    "Oct", "Nov", "Dec"
+];

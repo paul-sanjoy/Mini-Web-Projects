b9d69fc6ea744ad2f0e924da1e6a6884e366e0ae
paul-sanjoy <sanjoyworkspace@gmail.com>
2022-01-16 01:16:30 +0530

    added set-time functionality of the rotating clock

--- Themed-clock/script.js
+++ Themed-clock/script.js
@@ -10,6 +10,7 @@ const monthsName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep
     "Oct", "Nov", "Dec"
 ];
 
+
 toggle.addEventListener("click", function (event) {
     const html = document.querySelector("html");
     if (html.classList.contains("dark")) {
@@ -20,3 +21,38 @@ toggle.addEventListener("click", function (event) {
         event.target.innerHTML = "Light mode";
     }
 })
+
+
+//map a range of numbers to another range of numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (
+        ((num - in_min) * (out_max - out_min) / (in_max - in_min)) + out_min
+    );
+}
+
+//set time 
+const setTime = () => {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursOnClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const amPm = hours >= 12 ? "PM" : "AM";
+
+    hourEl.style.transform = `translate( -50%, -100%) rotate(${scale(hoursOnClock, 0 ,11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate( -50%, -100%) rotate(${scale(minutes, 0 ,59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate( -50%, -100%) rotate(${scale(seconds, 0 ,59, 0, 360)}deg)`;
+
+
+    
+
+
+}
+
+setTime();
+
+setInterval(setTime, 1000);
